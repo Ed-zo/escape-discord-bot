133ae55c2e785eeaf7f24545aa88ca1570584dc3
eskejpo <spam@minesite.eu>
2020-04-26 18:08:27 +0200

    Update config.js
    Config.js had data from old fork version of this bot

--- config.js
+++ config.js
@@ -2,9 +2,5 @@
 module.exports = {
     bot: {
         token: "",
-    },
-    golemagg: {
-        guild_id: "", //guild id for the role module to operate in
-        channel_id: "" //channel id for the role module to operate in
     }
 }
